4a29c84be7ecd0c5a0cd381a29f847770de9af89
ludwig.napigkit <ludwig.napigkit@intechsive.com>
2021-09-24 15:12:13 +0800

    login button on index

--- page_structure/header.php
+++ page_structure/header.php
@@ -23,3 +23,8 @@
 	</div>
 	<div class="container-fluid mb-5">
 		<a class="float-right btn btn-sm btn-light mb-2" href="">Refresh</a>
+		<?php
+			if(empty($_SESSION["usercode"])) {
+				echo '<a class="float-right btn btn-sm btn-success mb-2" href="">Login</a>';
+			}
+		?>
