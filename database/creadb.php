904bd998edd6e666ea05fec187747ffddbca1e39
ludwig.napigkit <ludwig.napigkit@intechsive.com>
2021-09-24 21:21:40 +0800

    if database exist

--- database/creadb.php
+++ database/creadb.php
@@ -1,13 +1,19 @@
 <?php
 
 	try {
-		$conn = new PDO("mysql:host=localhost;", "root", "");
-		$conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-		$sql = "CREATE DATABASE IF NOT EXISTS alpaphpdb";
-		$conn->exec($sql);
-		$sql = "use alpaphpdb";
-		$conn->exec($sql);
-		echo "Database: alpaphpdb; Successfully Created!<br>";
-	} catch(PDOException $e) {
-	    echo "Error".$e->getMessage()."<br>";
+		$pdo = new PDO('mysql:host=localhost;dbname=alpaphpdb', 'root', '', [ PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION ]);
+		echo '<a href="../">Continue..</a>';
+		die('Database Exist!');
+	} catch (PDOException $e) {
+		try {
+			$conn = new PDO("mysql:host=localhost;", "root", "");
+			$conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+			$sql = "CREATE DATABASE IF NOT EXISTS alpaphpdb";
+			$conn->exec($sql);
+			$sql = "use alpaphpdb";
+			$conn->exec($sql);
+			echo "Database: alpaphpdb; Successfully Created!<br>";
+		} catch(PDOException $e) {
+			echo "Error".$e->getMessage()."<br>";
+		}
 	}
