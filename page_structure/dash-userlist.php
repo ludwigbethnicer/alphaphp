cd58e3b08f80524258f6aad41e9c6073dd0a58c8
ludwig.napigkit <ludwig.napigkit@intechsive.com>
2021-09-25 00:55:54 +0800

    responsive table for user at dashboard

--- page_structure/dash-userlist.php
+++ page_structure/dash-userlist.php
@@ -18,52 +18,56 @@
 	
 ?>
 
-<div class="panel panel-default">
-	<div class="panel-heading">Users</div>
-	<div id="responseBody" class="panel-body table-responsive">
-		<table id="listUsers" class="table table-hover">
-			<thead>
-				<tr>
-					<th class="align-middle d-none d-lg-inline">ID</th>
-					<th class="align-middle">Username</th>
-					<th class="align-middle d-none d-lg-inline">Fullname</th>
-					<th class="align-middle d-none d-lg-inline">e-mail</th>
-					<th class="align-middle">Mobile No.</th>
-					<th class="align-middle d-none d-lg-inline">User Level</th>
-					<th class="align-middle d-none d-lg-inline">Created by</th>
-					<th class="align-middle">Online</th>
-					<th class="align-middle">Status</th>
-					<th class="align-middle d-none d-lg-inline">Date Created</th>
-					<th class="align-middle d-none d-lg-inline">Date Modified</th>
-					<th class="align-middle text-right">Action</th>
-				</tr>
-			</thead>
-			<tbody>
-			<?php
-				$query = "SELECT * FROM tbl_user";
-				$stmt = $dbcon->prepare($query);
-				$stmt->execute();
-				foreach ($stmt as $row) {
-					echo '<tr>';
-						echo '<td class="align-middle d-none d-lg-inline">'.$row['usercode'].'</td>';
-						echo '<td class="align-middle">'.$row['username'].'</td>';
-						echo '<td class="align-middle d-none d-lg-inline">'.$row['fullname'].'</td>';
-						echo '<td class="align-middle d-none d-lg-inline"><a href="mailto:'.$row['uemail'].'">'.$row['uemail'].'</a></td>';
-						echo '<td class=""><a href="//web.whatsapp.com/send?phone=%2B'.substr($row['umobileno'], 1, strlen($row['umobileno'])).'&text&app_absent=0" title="Send from WhatsApp" target="_blank">'.$row['umobileno'].'</a></td>';
-						echo '<td class="align-middle d-none d-lg-inline">'.$row['ulevpos'].'</td>';
-						echo '<td class="align-middle d-none d-lg-inline">'.$row['createdby'].'</td>';
-						echo '<td class="">'.$row['uonline'].'</td>';
-						echo '<td class="">'.$row['ustatz'].'</td>';
-						echo '<td class="align-middle d-none d-lg-inline">'.$row['created'].'</td>';
-						echo '<td class="align-middle d-none d-lg-inline">'.$row['modified'].'</td>';
-						echo '<td class="text-right">';
-							echo "<a class='btn btn-sm m-1 btn-success btn-inline' href='adduser.php?id=".$row['usercode']."'>View</a>";
-						echo '</td>';
-					echo '</tr>';
-				}
-			?>
-			</tbody>
-		</table>
+<div class="page-content">
+	<div class="container-fluid">
+		<div class="panel panel-default">
+			<div class="panel-heading">Users</div>
+			<div id="responseBody" class="panel-body table-responsive">
+				<table id="listUsers" class="table table-hover">
+					<thead>
+						<tr>
+							<th class="align-middle d-none d-lg-inline">ID</th>
+							<th class="align-middle">Username</th>
+							<th class="align-middle d-none d-lg-inline">Fullname</th>
+							<th class="align-middle d-none d-lg-inline">e-mail</th>
+							<th class="align-middle">Mobile No.</th>
+							<th class="align-middle d-none d-lg-inline">User Level</th>
+							<th class="align-middle d-none d-lg-inline">Created by</th>
+							<th class="align-middle">Online</th>
+							<th class="align-middle">Status</th>
+							<th class="align-middle d-none d-lg-inline">Date Created</th>
+							<th class="align-middle d-none d-lg-inline">Date Modified</th>
+							<th class="align-middle text-right">Action</th>
+						</tr>
+					</thead>
+					<tbody>
+					<?php
+						$query = "SELECT * FROM tbl_user";
+						$stmt = $dbcon->prepare($query);
+						$stmt->execute();
+						foreach ($stmt as $row) {
+							echo '<tr>';
+								echo '<td class="align-middle d-none d-lg-inline">'.$row['usercode'].'</td>';
+								echo '<td class="align-middle">'.$row['username'].'</td>';
+								echo '<td class="align-middle d-none d-lg-inline">'.$row['fullname'].'</td>';
+								echo '<td class="align-middle d-none d-lg-inline"><a href="mailto:'.$row['uemail'].'">'.$row['uemail'].'</a></td>';
+								echo '<td class=""><a href="//web.whatsapp.com/send?phone=%2B'.substr($row['umobileno'], 1, strlen($row['umobileno'])).'&text&app_absent=0" title="Send from WhatsApp" target="_blank">'.$row['umobileno'].'</a></td>';
+								echo '<td class="align-middle d-none d-lg-inline">'.$row['ulevpos'].'</td>';
+								echo '<td class="align-middle d-none d-lg-inline">'.$row['createdby'].'</td>';
+								echo '<td class="">'.$row['uonline'].'</td>';
+								echo '<td class="">'.$row['ustatz'].'</td>';
+								echo '<td class="align-middle d-none d-lg-inline">'.$row['created'].'</td>';
+								echo '<td class="align-middle d-none d-lg-inline">'.$row['modified'].'</td>';
+								echo '<td class="text-right">';
+									echo "<a class='btn btn-sm m-1 btn-success btn-inline' href='adduser.php?id=".$row['usercode']."'>View</a>";
+								echo '</td>';
+							echo '</tr>';
+						}
+					?>
+					</tbody>
+				</table>
+			</div>
+			<div class="panel-footer">List of Users.</div>
+		</div>
 	</div>
-	<div class="panel-footer">List of Users.</div>
 </div>
