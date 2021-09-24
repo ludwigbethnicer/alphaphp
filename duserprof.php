25fa73475637fb8ecd4416904501bb66cf662ebb
ludwig.napigkit <ludwig.napigkit@intechsive.com>
2021-09-24 15:24:34 +0800

    user auth

--- duserprof.php
+++ duserprof.php
@@ -2,6 +2,11 @@
 
 	include_once "config/core.php";
 	include_once "config/database.php";
+
+	if(!isset($_SESSION['username'])){
+		header('location:login.php');
+	}
+	
 	include_once "page_structure/dash-header.php";
 	include_once "page_structure/dash-userlist.php";
 	include_once "page_structure/dash-footer.php";
