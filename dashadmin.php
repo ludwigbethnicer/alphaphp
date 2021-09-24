d7541ee0f4f57bedca438cb36ad6f6f82be14847
ludwignapigkitintechsive <ludwig.napigkit@intechsive.com>
2021-09-24 12:16:29 +0800

    added admin dashborad forms

---
+++ dashadmin.php
@@ -0,0 +1,7 @@
+<?php 
+
+	include_once "config/core.php";
+	include_once "config/database.php";
+	include_once "view/dash-header.php";
+	include_once "view/dashadmin.php";
+	include_once "view/dash-footer.php";
---
+++ page_structure/header-preview.php
@@ -0,0 +1,67 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<meta http-equiv="refresh" content="3600;url=config/logout.php">
+	<title>IT Branch Audit Report</title>
+	<META HTTP-EQUIV="Pragma" CONTENT="no-cache">
+	<META HTTP-EQUIV="Expires" CONTENT="-1">
+	<style type="text/css">
+		body {
+			font-family: Tahoma, Geneva, sans-serif;
+			font-size: 11px;
+		}
+
+		table {
+			width: 100%;
+			max-width: 1074px;
+			border-collapse: collapse;
+			margin-left: auto;
+			margin-right: auto;
+			margin-bottom: 1rem;
+		}
+
+		.table-fluid {
+			max-width: unset;
+		}
+
+		table, tr, td, th {
+			border: 1px solid black;
+			vertical-align: middle;
+		}
+
+		b, p {
+			margin: 0;
+		}
+
+		.mxremk {
+			max-width: 132px;
+		}
+
+		.secdsip {
+			width: 28px;
+			text-align: center;
+		}
+
+		.unsecuredx {
+			background-color: #efacac;
+			color: red;
+		}
+
+		.notapplx {
+			background-color: #c6c7cc;
+			color: #8c675a;
+		}
+
+		.standcatg {
+			background-color: #a6bdc3;
+		}
+
+		.contwidth {
+			width: auto;
+			min-width: 242px;
+		}
+	</style>
+</head>
+<body>
