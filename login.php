5d3cf1e5c383a6c9d90799a43c7096d56ca2b5f9
ludwig.napigkit <ludwig.napigkit@intechsive.com>
2021-09-25 10:12:22 +0800

    page error

--- login.php
+++ login.php
@@ -2,4 +2,6 @@
 
 	include_once "config/core.php";
 	include_once "config/database.php";
+	include_once "page_structure/header.php";
 	include_once "page_structure/login.php";
+	include_once "page_structure/footer.php";
