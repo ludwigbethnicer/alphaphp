021f047d6690f69eecc48ac7aef55491ef540f82
ludwig.napigkit <ludwig.napigkit@intechsive.com>
2021-09-25 09:39:51 +0800

    dclknclk

--- login.php
+++ login.php
@@ -2,6 +2,4 @@
 
 	include_once "config/core.php";
 	include_once "config/database.php";
-	include_once "page_structure/header.php";
 	include_once "page_structure/login.php";
-	include_once "page_structure/footer.php";
